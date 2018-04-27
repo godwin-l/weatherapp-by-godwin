c1d394a2238cd0bdd3e909b571cc6245095bebc0
alloygodwin1 <34455677+alloygodwin1@users.noreply.github.com>
2018-04-27 13:04:28 +0530

    server
    edited js

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.post('/', function (req, res) {
     }
   });
 })
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+});
